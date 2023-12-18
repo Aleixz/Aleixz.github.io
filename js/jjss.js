878a9f165e639cd8241141ee4027e9d934a97832
Aleixz <104515147+Aleixz@users.noreply.github.com>
2023-12-19 00:18:30 +0800

    Update jjss.js

--- js/jjss.js
+++ js/jjss.js
@@ -49,45 +49,6 @@ document.querySelector('a[href="#one"]').addEventListener('click', function(even
   var header = document.querySelector('#one');
   header.scrollIntoView();
 });
-document.getElementById("date").textContent = "12/18";// 更新日期
-document.getElementById("date").style.color = "#41ff00";
-
-document.getElementById("version").textContent = "V164";// 更新版本号
-document.getElementById("version").style.color = "#ebff00";
-// 每日更新版链接
-document.getElementById("downloadButton1").addEventListener("click", function() {
-  redirect('https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTY2M1YxaWNuOW1iJnBhc3NDb2RlPTc2Ym4.jpg');
-});
-// 正式版链接
-document.getElementById("downloadButton2").addEventListener("click", function() {
-  redirect('https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTVTUk8xaWNycTdjJnBhc3NDb2RlPWFhZ3k.jpg');
-});
-// EA版链接
-document.getElementById("downloadButton3").addEventListener("click", function() {
-  redirect('https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTRGUTMxaWN1Z3hnJnBhc3NDb2RlPWFuMGI.jpg');
-});
-// 密钥链接
-document.getElementById("downloadButton4").addEventListener("click", function() {
-  redirect('https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaThQWTYxaWN1dzdn.jpg');
-});
-// 888以下
-// GPU链接1
-document.getElementById("bilibiliLink1").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTVNRk8xaWN0Zjhq.jpg";
-// GPU链接2
-document.getElementById("bilibiliLink2").href = "#";
-// GPU链接3
-document.getElementById("bilibiliLink3").href = "#";
-// GPU链接4
-document.getElementById("bilibiliLink4").href = "#";
-// 8GEN1以上
-// GPU链接1
-document.getElementById("bilibiliLink5").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTVNRk8xaWN0Zjhq.jpg";
-// GPU链接2
-document.getElementById("bilibiliLink6").href = "#";
-// GPU链接3
-document.getElementById("bilibiliLink7").href = "#";
-// GPU链接4
-document.getElementById("bilibiliLink8").href = "#";
 
 if (typeof Object.assign !== 'function') {
   Object.assign = function(target) {
