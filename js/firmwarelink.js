5d962899e8932821f85966cee10b6be631e68fef
Aleixz <104515147+Aleixz@users.noreply.github.com>
2023-12-21 16:32:11 +0800

    Update firmwarelink.js

--- js/firmwarelink.js
+++ js/firmwarelink.js
@@ -2,8 +2,9 @@
 // 版本号
 document.getElementById("01").textContent = "v17.0.0";
 // 设置链接
-document.getElementById("link01").href = "http://www.918.plus/down.php/907f7c99ba54ff84aba5dfc9e22df968.zip]Firmware 17.0.0.zip";
-document.getElementById("link01").target = "_blank";
+document.getElementById("link01").addEventListener("click", function() {
+  window.location.href = "http://www.918.plus/down.php/907f7c99ba54ff84aba5dfc9e22df968.zip]Firmware 17.0.0.zip";
+});
 
 // 设置字体大小和颜色
 document.getElementById("link01").style.fontSize = "16px";
@@ -13,8 +14,9 @@ document.getElementById("link01").style.color = "white";
 // 版本号
 document.getElementById("02").textContent = "v16.1.0";
 // 设置链接
-document.getElementById("link02").href = "http://www.918.plus/down.php/c32db52758a4bf6503869db531012e3d.zip]Firmware 16.1.0.zip";
-document.getElementById("link02").target = "_blank";
+document.getElementById("link02").addEventListener("click", function() {
+  window.location.href = "http://www.918.plus/down.php/c32db52758a4bf6503869db531012e3d.zip]Firmware 16.1.0.zip";
+});
 
 // 设置字体大小和颜色
 document.getElementById("link02").style.fontSize = "16px";
