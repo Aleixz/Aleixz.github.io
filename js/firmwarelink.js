de20d73865d2bf24411388fa4615debeb63f5cf5
Aleixz <104515147+Aleixz@users.noreply.github.com>
2023-12-21 16:06:31 +0800

    Add files via upload

---
+++ js/firmwarelink.js
@@ -0,0 +1,19 @@
+                        // 固件一号链接
+// 版本号
+document.getElementById("01").textContent = "v17.0.0";
+// 设置链接
+document.getElementById("link01").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2laMzY3MWlkeHR4aSZwYXNzQ29kZT01d3h4.jpg";
+
+// 设置字体大小和颜色
+document.getElementById("link01").style.fontSize = "16px";
+document.getElementById("link01").style.color = "white";
+
+                        // 固件二号链接
+// 版本号
+document.getElementById("02").textContent = "v16.1.0";
+// 设置链接
+document.getElementById("link02").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2laMzY3MWlkeHR4aSZwYXNzQ29kZT01d3h4.jpg";
+
+// 设置字体大小和颜色
+document.getElementById("link02").style.fontSize = "16px";
+document.getElementById("link02").style.color = "white";
