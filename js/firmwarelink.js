ef597bd9693636cfaea356b1910aea9cdb1bfb32
Aleixz <104515147+Aleixz@users.noreply.github.com>
2023-12-22 17:26:11 +0800

    Update firmwarelink.js

--- js/firmwarelink.js
+++ js/firmwarelink.js
@@ -6,10 +6,6 @@ document.getElementById("link01").addEventListener("click", function() {
   window.location.href = "http://www.918.plus/down.php/907f7c99ba54ff84aba5dfc9e22df968.zip]Firmware 17.0.0.zip";
 });
 
-// 设置字体大小和颜色
-document.getElementById("link01").style.fontSize = "16px";
-document.getElementById("link01").style.color = "white";
-
                         // 固件二号链接
 // 版本号
 document.getElementById("02").textContent = "v16.1.0";
@@ -17,7 +13,3 @@ document.getElementById("02").textContent = "v16.1.0";
 document.getElementById("link02").addEventListener("click", function() {
   window.location.href = "http://www.918.plus/down.php/c32db52758a4bf6503869db531012e3d.zip]Firmware 16.1.0.zip";
 });
-
-// 设置字体大小和颜色
-document.getElementById("link02").style.fontSize = "16px";
-document.getElementById("link02").style.color = "white";
