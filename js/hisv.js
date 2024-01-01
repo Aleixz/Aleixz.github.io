cf5f3ac7f4c8bb0e9645fbe6b4854352ccf12718
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-01-01 09:47:10 +0800

    更新 hisv.js

--- js/hisv.js
+++ js/hisv.js
@@ -100,6 +100,14 @@ document.getElementById("176").textContent = "v176";
 // 设置链接
 document.getElementById("link17").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lxVXkzMWpkZGs0YiZwYXNzQ29kZT1oZnly.jpg";
 
+// 九号链接
+// 版本号
+document.getElementById("177").textContent = "v177";
+// 设置链接
+document.getElementById("link18").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUxRTDAxamcxNXZlJnBhc3NDb2RlPTUzdG4.jpg";
+
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
