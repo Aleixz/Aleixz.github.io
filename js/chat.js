d666b3fd4d5a2ee288e03a9ec37235053a2e6b9d
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-01-02 23:24:18 +0800

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  var tidioScript = document.createElement('script');
+  tidioScript.src = '//code.tidio.co/aogzp7etcczy035l7jc1vs1p9dleuwku.js';
+  tidioScript.async = true;
+
+  document.body.appendChild(tidioScript);
+};
