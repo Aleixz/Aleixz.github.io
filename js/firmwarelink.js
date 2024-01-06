d9a36e3f287aac14ff11ffa86598b97051b0631f
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-01-06 18:30:13 +0800

    Update firmwarelink.js

--- js/firmwarelink.js
+++ js/firmwarelink.js
@@ -3,7 +3,7 @@
 document.getElementById("01").textContent = "v17.0.0";
 // 设置链接
 document.getElementById("link01").addEventListener("click", function() {
-  window.location.href = "http://www.918.plus/down.php/907f7c99ba54ff84aba5dfc9e22df968.zip]Firmware 17.0.0.zip";
+  window.location.href = "https://www.123pan.com/s/DCbbVv-1plHd.html";
 });
 
                         // 固件二号链接
