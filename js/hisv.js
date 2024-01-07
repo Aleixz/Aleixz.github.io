f550dd604a67561e0594b49b1e38421a2937fdfe
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-01-07 16:38:05 +0800

    Update hisv.js

--- js/hisv.js
+++ js/hisv.js
@@ -1,154 +1,37 @@
-                        // 一号链接
-// 版本号
-document.getElementById("1").textContent = "v165";
-// 设置链接
-document.getElementById("link1").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2laMzY3MWlkeHR4aSZwYXNzQ29kZT01d3h4.jpg";
+  var panelHTML = `
+    <p class="special-p"><span style="color: #dcff00;">点击以下任意版本开始下载</span></p>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVNXOVQxa2JybHZjJnBhc3NDb2RlPWJxb2I.jpg">v185</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lMeGZnMWs3cjVwZyZwYXNzQ29kZT1kYm8y.jpg">v184</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTVNNFMxazNkMTBqJnBhc3NDb2RlPWR4b3c.jpg">v183</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTJLSzkxazJzemhlJnBhc3NDb2RlPTh6ZXU.jpg">v182</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUU1TVMxanlzYzJoJnBhc3NDb2RlPTl4emg.jpg">v181</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTE5Qlcxam9oeWJjJnBhc3NDb2RlPWJkcXc.jpg">v179</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUdOSjUxampsZzJqJnBhc3NDb2RlPWh4c2Q.jpg">v178</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUxRTDAxamcxNXZlJnBhc3NDb2RlPTUzdG4.jpg">v177</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lxVXkzMWpkZGs0YiZwYXNzQ29kZT1oZnly.jpg">v176</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVNGRjgxajlwc2ZpJnBhc3NDb2RlPWJ1MnQ.jpg">v175</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVdMUDAxajZkazhkJnBhc3NDb2RlPTdwOHM.jpg">v174</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTNJWTkxajJsNWhnJnBhc3NDb2RlPTNmMmc.jpg">v173</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lqU01vMWowNXBnaCZwYXNzQ29kZT04djI4.jpg">v172</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lWWU02MWl3a28wYiZwYXNzQ29kZT04amlw.jpg">v171</a>
+		<a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUs1NUExaXQwaW5hJnBhc3NDb2RlPTRldjA.jpg">v170</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUEyN0MxaXBvOHpjJnBhc3NDb2RlPTNjdXU.jpg">v169</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2kzUmhoMWluOWhkYyZwYXNzQ29kZT03ejlu.jpg">v168</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUtZWkcxaWsybjdjJnBhc3NDb2RlPTh3dXA.jpg">v167</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTJISE4xaWgzbHNqJnBhc3NDb2RlPWI1a3o.jpg">v166</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2laMzY3MWlkeHR4aSZwYXNzQ29kZT01d3h4.jpg">v165</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2k2NjNWMWljbjltYiZwYXNzQ29kZT03NmJu.jpg">v164</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lURklFMWlqYTE2YiZwYXNzQ29kZT0za3Rn.jpg">v163</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lMWFY2MWlqYTVtYiZwYXNzQ29kZT1lbGI2.jpg">v162</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lXMjExMWlqYTRyYSZwYXNzQ29kZT04NThu.jpg">v161</a>
+    <a class="w3-button w3-block w3-left-align" href="https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lDMllZMWlqYTV2YSZwYXNzQ29kZT1icWJr.jpg">v160</a>
+  `;
 
-                        // 二号链接
-// 版本号
-document.getElementById("2").textContent = "v164";
-// 设置链接
-document.getElementById("link2").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2k2NjNWMWljbjltYiZwYXNzQ29kZT03NmJu.jpg";
+  // 获取 panel 元素
+  var panelElement = document.querySelector('.panel');
 
-                        // 三号链接
-// 版本号
-document.getElementById("3").textContent = "v163";
-// 设置链接
-document.getElementById("link3").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lURklFMWlqYTE2YiZwYXNzQ29kZT0za3Rn.jpg";
-
-                        // 四号链接
-// 版本号
-document.getElementById("4").textContent = "v162";
-// 设置链接
-document.getElementById("link4").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lMWFY2MWlqYTVtYiZwYXNzQ29kZT1lbGI2.jpg";
-
-                        // 五号链接
-// 版本号
-document.getElementById("5").textContent = "v161";
-// 设置链接
-document.getElementById("link5").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lXMjExMWlqYTRyYSZwYXNzQ29kZT04NThu.jpg";
-
-                        // 六号链接
-// 版本号
-document.getElementById("6").textContent = "v160";
-// 设置链接
-document.getElementById("link6").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lDMllZMWlqYTV2YSZwYXNzQ29kZT1icWJr.jpg";
-
-                        // 七号链接
-// 版本号
-document.getElementById("166").textContent = "v166";
-// 设置链接
-document.getElementById("link7").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTJISE4xaWgzbHNqJnBhc3NDb2RlPWI1a3o.jpg";
-
-// 八号链接
-// 版本号
-document.getElementById("167").textContent = "v167";
-// 设置链接
-document.getElementById("link8").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUtZWkcxaWsybjdjJnBhc3NDb2RlPTh3dXA.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("168").textContent = "v168";
-// 设置链接
-document.getElementById("link9").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2kzUmhoMWluOWhkYyZwYXNzQ29kZT03ejlu.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("169").textContent = "v169";
-// 设置链接
-document.getElementById("link10").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUEyN0MxaXBvOHpjJnBhc3NDb2RlPTNjdXU.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("170").textContent = "v170";
-// 设置链接
-document.getElementById("link11").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUs1NUExaXQwaW5hJnBhc3NDb2RlPTRldjA.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("171").textContent = "v171";
-// 设置链接
-document.getElementById("link12").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lWWU02MWl3a28wYiZwYXNzQ29kZT04amlw.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("172").textContent = "v172";
-// 设置链接
-document.getElementById("link13").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lqU01vMWowNXBnaCZwYXNzQ29kZT04djI4.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("173").textContent = "v173";
-// 设置链接
-document.getElementById("link14").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTNJWTkxajJsNWhnJnBhc3NDb2RlPTNmMmc.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("174").textContent = "v174";
-// 设置链接
-document.getElementById("link15").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVdMUDAxajZkazhkJnBhc3NDb2RlPTdwOHM.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("175").textContent = "v175";
-// 设置链接
-document.getElementById("link16").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVNGRjgxajlwc2ZpJnBhc3NDb2RlPWJ1MnQ.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("176").textContent = "v176";
-// 设置链接
-document.getElementById("link17").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lxVXkzMWpkZGs0YiZwYXNzQ29kZT1oZnly.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("177").textContent = "v177";
-// 设置链接
-document.getElementById("link18").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUxRTDAxamcxNXZlJnBhc3NDb2RlPTUzdG4.jpg";
-
-
-// 九号链接
-// 版本号
-document.getElementById("178").textContent = "v178";
-// 设置链接
-document.getElementById("link19").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUdOSjUxampsZzJqJnBhc3NDb2RlPWh4c2Q.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("179").textContent = "v179";
-// 设置链接
-document.getElementById("link20").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTE5Qlcxam9oeWJjJnBhc3NDb2RlPWJkcXc.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("181").textContent = "v181";
-// 设置链接
-document.getElementById("link21").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUU1TVMxanlzYzJoJnBhc3NDb2RlPTl4emg.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("182").textContent = "v182";
-// 设置链接
-document.getElementById("link22").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTJLSzkxazJzemhlJnBhc3NDb2RlPTh6ZXU.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("183").textContent = "v183";
-// 设置链接
-document.getElementById("link23").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaTVNNFMxazNkMTBqJnBhc3NDb2RlPWR4b3c.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("184").textContent = "v184";
-// 设置链接
-document.getElementById("link24").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lMeGZnMWs3cjVwZyZwYXNzQ29kZT1kYm8y.jpg";
-
-// 九号链接
-// 版本号
-document.getElementById("185").textContent = "v185";
-// 设置链接
-document.getElementById("link25").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVNXOVQxa2JybHZjJnBhc3NDb2RlPWJxb2I.jpg";
-
+  // 插入生成的 HTML 代码
+  panelElement.innerHTML = panelHTML;
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -164,7 +47,6 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
-
 // 自动刷新js
 if (typeof Object.assign !== 'function') {
   Object.assign = function(target) {
