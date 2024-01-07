8fa32de1f000a98d89101d1a23f3c47cbd437b97
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-01-07 09:35:52 +0800

    更新 link.js

--- js/link.js
+++ js/link.js
@@ -1,12 +1,12 @@
-document.getElementById("date").textContent = "1/6";// 更新日期
+document.getElementById("date").textContent = "1/7";// 更新日期
 document.getElementById("date").style.color = "#41ff00";
 
-document.getElementById("version").textContent = "V184";// 更新版本号
+document.getElementById("version").textContent = "V185";// 更新版本号
 document.getElementById("version").style.color = "#ebff00";
 
 // 每日更新版链接
 document.getElementById("downloadButton1").addEventListener("click", function() {
-  redirect('https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2lMeGZnMWs3cjVwZyZwYXNzQ29kZT1kYm8y.jpg');
+  redirect('https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVNXOVQxa2JybHZjJnBhc3NDb2RlPWJxb2I.jpg');
 });
 // 正式版链接
 document.getElementById("downloadButton2").addEventListener("click", function() {
