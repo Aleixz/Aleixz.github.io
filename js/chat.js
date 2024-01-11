40fa45136e34b95d9099603ed86b1734a06fe9f1
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-01-11 09:57:29 +0800

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -2,6 +2,11 @@ window.onload = function() {
   var tidioScript = document.createElement('script');
   tidioScript.src = '//code.tidio.co/aogzp7etcczy035l7jc1vs1p9dleuwku.js';
   tidioScript.async = true;
-
   document.body.appendChild(tidioScript);
+  
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-TNM6VLRK0K');
 };
+
