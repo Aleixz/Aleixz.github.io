50c8563e7717205405cd9198c404983ce56fee40
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-01-18 21:31:12 +0800

    Delete js/chat.js

--- js/chat.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  var tidioScript = document.createElement('script');
-  tidioScript.src = '//code.tidio.co/aogzp7etcczy035l7jc1vs1p9dleuwku.js';
-  tidioScript.async = true;
-
-  document.body.appendChild(tidioScript);
-};
-
