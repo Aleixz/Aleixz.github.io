3fdc5a82b7327fd65303d16f446a777bfa8bfaaf
StarOnTheSky <i@sots.eu.org>
2024-01-25 23:41:13 +0800

    实现 hisv.html 的自动更新

--- gen.py
+++ gen.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 
-# 读取 index.html
+### 处理 index.html
 with open('index.html', 'r') as f:
     html = f.read()
 
@@ -34,3 +34,35 @@ soup.find(id='downloadButton').attrs['onclick'] = "window.location.href='" + apk
 with open('index.html', 'w') as f:
     f.write(str(soup.prettify()))
 
+
+### 处理 hisv.html
+with open('hisv.html', 'r') as f:
+    html = f.read()
+
+# 解析 hisv.html
+soup = BeautifulSoup(html, 'html.parser')
+
+# 清空 releasesList
+soup.find('ul', id='releasesList').clear()
+
+# 读取历史版本
+for releases in data:
+    ver = releases['name'].replace('android-', '')
+    date = datetime.datetime.strptime(releases['published_at'], '%Y-%m-%dT%H:%M:%SZ')
+    formattedDate = str(date.year) + '年' + str(date.month) + '月' + str(date.day) + '日'
+    for objs in releases['assets']:
+        if objs['name'].endswith('.apk'):
+            apk_url = objs['browser_download_url']
+            break
+    apk_url = apk_url.replace('https://github.com', 'https://hub.fgit.cf')
+    # 生成 html
+    new_li = soup.new_tag('li')
+    new_a = soup.new_tag('a')
+    new_a['href'] = apk_url
+    new_a.string = formattedDate + '    ' + releases['name']
+    new_li.append(new_a)
+    soup.find('ul', id='releasesList').append(new_li)
+
+# 保存 html
+with open('hisv.html', 'w') as f:
+    f.write(str(soup.prettify()))
