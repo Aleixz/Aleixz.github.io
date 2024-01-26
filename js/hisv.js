67afe5170ab666e446d6d9ae199a47f190ff867d
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-01-26 18:58:47 +0800

    Add files via upload

--- js/hisv.js
+++ js/hisv.js
@@ -38,16 +38,50 @@ function displayReleases() {
   const releasesList = document.getElementById('releasesList');
   const loadingStatus = document.getElementById('loadingStatus');
   const accordions = document.getElementsByClassName('accordion');
-  // 手动触发刷新手风琴状态
-  Array.from(accordions).forEach(accordion => {
-    if (accordion.classList.contains('active')) {
-      accordion.nextElementSibling.style.maxHeight = accordion.nextElementSibling.scrollHeight + "px";
-    } else {
-      accordion.nextElementSibling.style.maxHeight = null;
-    }
+
+fetch('https://api.github.com/repos/yuzu-emu/yuzu-android/releases?per_page=35', {
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28',
+    'Accept': 'application/vnd.github+json',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0'
+  }
+})
+.then(response => response.json())
+.then(data => {
+  const releases = data.slice(0, 35).map(release => {
+    // 查找名为"yuzu-*.apk"的附件，并获取对应的下载链接
+    const apkAsset = release.assets.find(asset => asset.name.endsWith('.apk'));
+    return {
+      tag: release.tag_name,
+      downloadUrl: apkAsset ? apkAsset.browser_download_url : null
+    };
+  });
+
+    releases.forEach(release => {
+      const listItem = document.createElement('li');
+      const link = document.createElement('a');
+      link.href = release.downloadUrl;
+      link.textContent = release.tag;
+      listItem.appendChild(link);
+      releasesList.appendChild(listItem);
+    });
+
+    // 隐藏加载状态
+    loadingStatus.style.display = 'none';
+
+    // 手动触发刷新手风琴状态
+    Array.from(accordions).forEach(accordion => {
+      if (accordion.classList.contains('active')) {
+        accordion.nextElementSibling.style.maxHeight = accordion.nextElementSibling.scrollHeight + "px";
+      } else {
+        accordion.nextElementSibling.style.maxHeight = null;
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
   });
 }
-  
 
 window.addEventListener('load', displayReleases);
 
