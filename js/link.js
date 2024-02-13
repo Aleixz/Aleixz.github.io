58ae2db23ac31cfef7daa572d2b24c369c4e8f30
Aleixz <104515147+Aleixz@users.noreply.github.com>
2024-02-13 14:42:29 +0800

    Update link.js

--- js/link.js
+++ js/link.js
@@ -10,7 +10,7 @@ document.getElementById("downloadButton3").addEventListener("click", function() 
 // GPU链接1
 document.getElementById("bilibiliLink1").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVBDWlIxbzRsbWNm.jpg";
 // GPU链接2
-document.getElementById("bilibiliLink2").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2k0TzgwMWx4a2hyYQ.jpg";
+document.getElementById("bilibiliLink2").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaW4xU3QxbzVicDBo.jpg";
 // GPU链接3
 document.getElementById("bilibiliLink3").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaU5FNlMxaWRpNXpn.jpg";
 // GPU链接4
@@ -19,7 +19,7 @@ document.getElementById("bilibiliLink4").href = "https://dlink.host/lanzou/aHR0c
 // GPU链接1
 document.getElementById("bilibiliLink5").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaVBDWlIxbzRsbWNm.jpg";
 // GPU链接2
-document.getElementById("bilibiliLink6").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3lyLmxhbnpvdXUuY29tL2k0TzgwMWx4a2hyYQ.jpg";
+document.getElementById("bilibiliLink6").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaW4xU3QxbzVicDBo.jpg";
 // GPU链接3
 document.getElementById("bilibiliLink7").href = "https://dlink.host/lanzou/aHR0cHM6Ly93d3AubGFuem91cC5jb20vaUlDNDYxaWRpNXBn.jpg";
 // GPU链接4
